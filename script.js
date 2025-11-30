288ad65bb4d97d6d38d32b88598bb2d4a6ce2977
Puneet Arora <arorapuneet234@gmail.com>
2025-11-30 17:55:08 +0530

    Randomize color feature added

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ for(let i=0;i<num*num;i++){
  container.appendChild(div)
  div.style.width=`${640/num}px`;
  div.style.height=`${640/num}px`;
- 
  div.classList.add("child");
+
+ const randomize=document.querySelector("#randomize");
+ randomize.addEventListener("click",fn2);
+ function fn2(){
+    let red=Math.random()*256;
+    let green=Math.random()*256;
+    let blue=Math.random()*256;
+    div.addEventListener("mouseenter", (e)=>{ e.target.style.backgroundColor =`rgba(${red}, ${green}, ${blue}, 1)`})
+ }
  }
 
 }
@@ -22,6 +30,7 @@ for(let i=0;i<num*num;i++){
 
 
  const sqrs=document.querySelector("#sqrs");
+ 
  sqrs.addEventListener("click",fn);
  function fn(e){
     let num = prompt("Enter value from 1 to 100");
